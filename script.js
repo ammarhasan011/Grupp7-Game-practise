f561c9766f00fecfb1c16ae449896804ae165220
AjshaC <94913424+AjshaC@users.noreply.github.com>
2022-10-11 15:41:57 +0200

    ComputerFunction

--- script.js
+++ script.js
@@ -1,42 +1,89 @@
-
-const text = document.querySelector("h1")
-const buttons = document.querySelector(".buttons")
+const text = document.querySelector("h1");
+const buttons = document.querySelector(".buttons");
 const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
 
-rock.addEventListener("click", check);
-paper.addEventListener("click", check);
-scissors.addEventListener("click", check);
-
+rock.addEventListener("click", Rock);
+paper.addEventListener("click", Paper);
+scissors.addEventListener("click", Scissors);
 
 //optionComputer
 
-function check(player) {
-    if (player === "rock"){
-        tie();
+function ComputerPlayer() {
+  var randomNum = Math.floor(Math.random() * 2);
+
+  if (randomNum == 0) {
+    return "rock";
+  }
+  if (randomNum == 1) {
+    return "paper";
+  } else if (randomNum == 2) {
+    return "scissors";
+  }
+}
+
+function Play(ComputerPlayer, Player) {
+  if (ComputerPlayer == Player) {
+    return "tie";
+  }
+  if (ComputerPlayer == "rock") {
+    if (Player == "paper") {
+      return "Win";
+    } else {
+      return "You lose";
     }
-    if(player === "paper"){
-        win();
+  }
+
+  if (ComputerPlayer == "paper") {
+    if (Player == "scissors") {
+      return "Win";
+    } else {
+      return "You Lose";
     }
-    else if (player ==="scissors") {
-        lose();
+  }
+
+  if (ComputerPlayer == scissors) {
+    if (Player == "rock") {
+      return "Win";
+    } else {
+      return "You lose";
     }
-    console.log("hej");
+  }
 }
 
+console.log(ComputerPlayer());
 
-
-function tie(){
-    text.innertext = "Its a tie!!!";
+function Rock() {
+  text.innerHTML = "Rock";
 }
 
-
-function win() {
-    text.innertext = "Its a wiiiiiin!!!";
+function Paper() {
+  text.innerHTML = "Paper";
 }
 
-
-function lose() {
-    text.innertext = "you looooose!!!";
+function Scissors() {
+  text.innerHTML = "Scissors";
 }
+//   if (player === "rock") {
+//     tie();
+//   }
+//   if (player === "paper") {
+//     win();
+//   } else if (player === "scissors") {
+//     lose();
+//   }
+//   console.log("hej");
+// }
+
+// function tie() {
+//   text.innertext = "Its a tie!!!";
+// }
+
+// function win() {
+//   text.innertext = "Its a wiiiiiin!!!";
+// }
+
+// function lose() {
+//   text.innertext = "you looooose!!!";
+// }
